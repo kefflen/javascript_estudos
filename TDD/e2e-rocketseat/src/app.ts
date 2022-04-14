818d849f0583424677b1ff903cfa6e40cdab6b64
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2022-04-14 19:37:57 -0300

    Implementando rota createLesson

--- TDD/e2e-rocketseat/src/app.ts
+++ TDD/e2e-rocketseat/src/app.ts
@@ -1,9 +1,21 @@
 import express from 'express'
+import { PrismaLessonRepository } from './repositories/prisma/PrismaLessonsRepository'
+import { CreateLesson } from './services/CreateLesson'
 const app = express()
 
 app.use(express.json)
 
-app.post('/lesson', (request, response) => {
+app.post('/lesson', async (request, response) => {
+  const { title, description } = request.body
+
+  const prismaLessonRepository = new PrismaLessonRepository()
+  const createLesson = new CreateLesson(prismaLessonRepository)
+
+  try {
+    await createLesson.execute({ title, description })
+  } catch (e: any) {
+    return response.status(400).json({ error: e.message })
+  }
   return response.status(201).send()
 })
 
