2fd0b2db34f9694bf739165b8ac1c118d3b691b4
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2022-06-18 20:27:20 -0300

    slate

--- React/rich-text/plate/src/components/Editor/index.tsx
+++ React/rich-text/plate/src/components/Editor/index.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
-import { createEditor } from 'slate'
+import React, { useCallback, useState } from 'react'
+import { createEditor, Transforms, Editor as _Editor } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
 import { BaseEditor, Descendant } from 'slate'
-import { ReactEditor } from 'slate-react'
+import { ReactEditor} from 'slate-react'
 
 import { Container } from "./styled";
 
-type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomElement = { type: 'paragraph'|'code'; children: CustomText[] }
 type CustomText = { text: string }
 
 declare module 'slate' {
@@ -24,13 +24,54 @@ const initialValue: Descendant[] = [
   },
 ]
 
+type plateElement = {
+  attributes: any,
+  children: any
+}
+
+const CodeElement: React.FC<plateElement> = props => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  )
+}
+
+const DefaultElement: React.FC<plateElement> = props => {
+  return <p {...props.attributes}>{props.children}</p>
+}
+
 const Editor: React.FC = () => {
   const [editor] = useState(() => withReact(createEditor()))
+
+  const renderElement = useCallback((props: any) => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
 
   return (
     <Container>
       <Slate editor={editor} value={initialValue}>
-        <Editable />
+        <Editable 
+          renderElement={renderElement}
+          onKeyDown={event => {
+            if (event.key === ';' && event.ctrlKey) {
+              console.log('Entrou')
+              // Prevent the "`" from being inserted by default.
+              event.preventDefault()
+              // Otherwise, set the currently selected blocks type to "code".
+              Transforms.setNodes(
+                editor,
+                { type: 'code' },
+                { match: n => _Editor.isBlock(editor, n) }
+              )
+            }
+          }}
+        />
       </Slate>
     </Container>
   )
