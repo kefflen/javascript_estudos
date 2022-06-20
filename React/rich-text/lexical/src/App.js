99605e7329734fd6492802cb177bd1b7d583d3fd
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2022-06-19 22:19:27 -0300

    Basic lexical component

---
+++ React/rich-text/lexical/src/App.js
@@ -0,0 +1,11 @@
+import BasicLexical from "./components/BasicLexical";
+
+function App() {
+  return (
+    <div className="App">
+      <BasicLexical />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/rich-text/lexical/src/components/BasicLexical/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  margin: 1.5rem;
+  padding: 0.5rem;
+  box-shadow: 1px 1px 0.5em;
+`
